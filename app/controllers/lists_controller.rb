227ec90deb7c6a31aa72bfedaf30ae3f39f0d7db
toshizo0405 <113435257+toshizo0405@users.noreply.github.com>
2023-06-18 02:38:33 +0000

    [Updete8]

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -36,9 +36,9 @@ class ListsController < ApplicationController
   end
 
   def destroy
-   list = List.find(params[:id])
-   list.destroy
-   redirect_to '/lists'
+    list =List.find(params[:id])
+    list.destroy
+    redirect_to '/lists'
   end
 
   private
